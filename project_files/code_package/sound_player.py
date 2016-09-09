75f7633a3b5afe723361cd33b04e4ef80f15fc7d
Thi Yen Thu Nguyen <thi-yen-thu.nguyen@hsrw.org>
2016-09-10 00:14:43 +0200

    Delete sound_player.py

--- project_files/code_package/sound_player.py
+++
@@ -1,8 +0,0 @@
-from os import system as s
-
-def play_sound(file, volume=0):
-    s('omxplayer --vol ' + str(volume) + ' sound-files/' + str(file))
-
-
-play_sound('hello.wav','80')
-play_sound('applause3.mp3','-800')
